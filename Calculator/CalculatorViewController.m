2d1a0211c930e229f4659cce85c75949ab355923
Timothy Shiu <timoshiu@gmail.com>
2012-05-15 02:49:29 -0700

    hook up the MVC ViewController with the View and Model.

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,12 +7,68 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorModel.h"
+
+@interface CalculatorViewController()
+
+@property (nonatomic,strong) CalculatorModel* model;
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+@end
 
 @implementation CalculatorViewController
+
+@synthesize model = _model;
+@synthesize display = _display;
+
+- (void) setModel:(CalculatorModel *)model
+{
+    _model = model;
+}
+
+- (CalculatorModel*) getModel
+{
+    if (_model != nil) { return _model; }
+    _model = [[CalculatorModel alloc] init];
+    return _model;
+}
+
+- (void) updateDisplay:(id) obj
+{
+    if (obj == nil) { return; }
+    BOOL ok = [[obj class] isSubclassOfClass:[NSNumber class]];
+    if (!ok) { return; }
+    NSNumber* num = (NSNumber*) obj;
+    UILabel *label = self.display;
+    [label setText:[num stringValue]];
+}
+
+- (IBAction)pressOperator:(id)sender 
+{
+    if (sender == nil) { return; }
+    NSLog(@"%@ is pressed\n", sender);
+    BOOL ok = [[sender class] isSubclassOfClass:[UIButton class]];
+    if (!ok) { return; }
+    UIButton* button = (UIButton*) sender;
+    UILabel* label = [button titleLabel];
+    NSString* operator = [label text];
+    id result = [[self getModel] push:operator];
+    [self updateDisplay:result];
+}
 
 - (IBAction)pressDigit:(id)sender 
 {
     NSLog(@"%@ is pressed\n", sender);
+    if ([[sender class] isSubclassOfClass:[UIButton class]]) {
+        UIButton* button = (UIButton*) sender;
+        UILabel* label = [button titleLabel];
+        NSString* string = [label text];
+        int value = [string intValue];
+        NSNumber* digit = [NSNumber numberWithInt:value];
+        [[self getModel] push:digit];
+        [self updateDisplay:digit];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,6 +87,7 @@
 
 - (void)viewDidUnload
 {
+    [self setDisplay:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
